961177828fd1c81a38b8588203d22f54f2a0de79
VadimTeslyuk <eryilyvatar@gmail.com>
2024-10-14 21:37:38 +0300

    add attach

---
+++ src/test/java/RegistrationWithDemoQAPage.java
@@ -0,0 +1,42 @@
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationWithDemoQAPage extends TestBase {
+
+    @Test
+    @Feature("Заполнение формы demoqa")
+    @Story("Форма demoqa")
+    @Owner("teslyukvv")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "DemoQA", url = "https://demoqa.com")
+    @DisplayName("Проверка заполнения формы demoqa")
+    void formVerification() {
+        registrationPage.openPage()
+                .killReclam()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(userEmail)
+                .setGender(userGender)
+                .setNumber(userNumber)
+                .setBirthDate(dayBirthUser, monthBirthUser, yearBirthUser)
+                .setUserSubjects(userSubjects)
+                .setUserHobbie(hobbie1, hobbie2, hobbie3)
+                .setUserPicture(pictureUrl)
+                .setUserAddres(userAddress)
+                .setUserState(userState)
+                .setUserCity(userCity)
+                .submitUserData()
+                .verifyResultModalAppears()
+                .verifyResultData("Student Name", firstName + " " + lastName)
+                .verifyResultData("Student Email", userEmail)
+                .verifyResultData("Gender", userGender)
+                .verifyResultData("Mobile", userNumber)
+                .verifyResultData("Date of Birth", dayBirthUser + " " + monthBirthUser + "," + yearBirthUser)
+                .verifyResultData("Hobbies", hobbie1 + ", " + hobbie2 + ", " + hobbie3)
+                .verifyResultData("Picture", pictureUrl)
+                .verifyResultData("Address", userAddress)
+                .verifyResultData("State and City", userState + " " + userCity);
+    }
+}
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -19,9 +21,9 @@ public class TestBase extends TestData{
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
+//        Configuration.browserVersion = "100.0";
         Configuration.browserSize = "2560x1440";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -35,5 +37,13 @@ public class TestBase extends TestData{
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+//        Attach.video();
     }
 }
